72b116aaadbf25661867a62b6281ed85ec366eb8
MaingiMark <mark.maingi@student.moringaschool.com>
2025-09-09 08:55:31 +0300

    used arrow functions

---
+++ index.js
@@ -0,0 +1,16 @@
+let a = 2000;
+let b = 100;
+const divide = (a, b) => {
+    return a / b;
+
+}
+
+const square = (a) => {
+    return a * a;
+}
+
+
+const add = (a, b) => {
+    return a + b;
+
+}
